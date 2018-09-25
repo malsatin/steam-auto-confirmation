1b0cfd41478e65f25d87b3751f0b5c966cf1cd82
Sergey <sergejrf@gmail.com>
2018-09-25 20:35:12 +0300

    Added ability to check multiple bots at once

--- index.js
+++ index.js
@@ -28,22 +28,63 @@ function prepareConfig(rawConfig) {
     };
 
     let config = JSON.parse(rawConfig);
-    requiredProps.forEach((prop) => {
-        if(!config.hasOwnProperty(prop) || !config[prop].length) {
-            throw new Error("Config should contain non-empty field " + prop);
-        }
-    });
+    if(!Array.isArray(config)) {
+        config = [config];
+    }
 
-    return merge({}, defaultProps, config);
+    for(let i = 0; i < config.length; i++) {
+        let conf = config[i];
+        requiredProps.forEach((prop) => {
+            if(!conf.hasOwnProperty(prop) || !conf[prop].length) {
+                throw new Error("Config should contain non-empty field " + prop);
+            }
+        });
+
+        config[i] = merge({}, defaultProps, conf);
+    }
+
+    return config;
+}
+
+function startNextBot() {
+    let conf = config[conf_i];
+    if(!conf) {
+        log.info("# All bots checked");
+        return;
+    }
+
+    bot = new Bot(conf, log);
+    bot.start();
+    conf_i++;
+
+    let checker = setInterval(() => {
+        if(bot._initialized && bot.hasNoConfirmations()) {
+            clearInterval(checker);
+            bot.stop();
+
+            log.info("Switching bot..");
+
+            startNextBot();
+        }
+    }, 1000);
+}
+
+function startOneBot() {
+    bot = new Bot(config[0], log);
+    bot.start();
 }
 
 let rawConfig = fs.readFileSync("./config.json");
 let config = prepareConfig(rawConfig);
 
-log.log("Config loaded");
+log.info("Config loaded");
 
-let bot = new Bot(config, log);
-bot.start();
+let conf_i = 0, bot;
+if(config.length > 1) {
+    startNextBot();
+} else {
+    startOneBot();
+}
 
 process.on("SIGINT", function() {
     log.info("Stopping bot..");
